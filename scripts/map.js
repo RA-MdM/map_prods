808a9eb615fd7f00cf8cb8db655e7670732fb3bb
RA-MdM <90396485+RA-MdM@users.noreply.github.com>
2021-09-09 17:22:12 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -130,7 +130,7 @@ $(window).on('load', function() {
           .bindPopup("<strong>" + point['Name'] + '</strong><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + '<b>Description : </b>' + 
           point['Description']+ '</b><br>' + '<b>Location : </b>' + point['Location'] + '</b><br>' + 
-                     '<b>Budget : </b>' + point['Budget'] + '</b><br>' + '<b>Année : </b>' + point['Year'] + '</b><br>' + '<b>Link : </b>' + point['Link']
+                     '<b>Budget : </b>' + point['Budget'] + '</b><br>' + '<b>Année : </b>' + point['Year'] + '</b><br>' + '<b>Link : </b>' + point['Link2']
                     + '</b><br>' + '<b>Ref : </b>' + point['Ref']);
 
         if (layers !== undefined && layers.length !== 1) {
